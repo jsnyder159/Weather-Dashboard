d321c5db5a93284c90bcc27514589187b2c3c93c
Jason Snyder <jason.snyder159@gmail.com>
2022-03-03 18:30:16 -0500

    added api's and basic layout format

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+
+let weatherInfo = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={6212f09690f4fd274b9ba1da9141cbaa}"
+let locationInfo = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={6212f09690f4fd274b9ba1da9141cbaa}"
