6c1c2884a731e16bfc928ec3a72db9effc53981e
Jason Snyder <jason.snyder159@gmail.com>
2022-03-03 22:08:02 -0500

    got local storage saving and displaying information

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,36 @@
 
-let weatherInfo = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={6212f09690f4fd274b9ba1da9141cbaa}"
+let weatherInfo = "https://api.openweathermap.org/data/2.5/onecall?lat=&lon={lon}&exclude={part}&appid={6212f09690f4fd274b9ba1da9141cbaa}"
 let locationInfo = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={6212f09690f4fd274b9ba1da9141cbaa}"
+let inputField = document.querySelector(".input")
+let searchBtn = document.querySelector(".button")
+let savedCity = document.querySelector(".city-searched")
+
+// function savedSearch() {
+searchBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    let savedSearchedCity = localStorage.getItem("searchedCity");
+    console.log(savedSearchedCity)
+    let searchedCity = []
+    if (savedSearchedCity === null) {
+        searchedCity = []
+
+    } else {
+        searchedCity = JSON.parse(localStorage.getItem("searchedCity"));
+
+    }
+    let input = inputField.value
+    searchedCity.push(input);
+    localStorage.setItem("searchedCity", JSON.stringify(searchedCity));
+
+showSearched();
+})
+
+
+function showSearched() {
+    let searched = JSON.parse(localStorage.getItem("searchedCity"));
+    searched.forEach(function (i) {
+        let li = document.createElement("li");
+        li.textContent = i
+        savedCity.append(li)
+    })
+}
