3394a6a90c10510d10cc8ed357d97bbdd7528a77
Jason Snyder <jason.snyder159@gmail.com>
2022-03-06 01:04:38 -0500

    got weather icons working and added small boxes for future weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,10 @@
-
-// let weatherInfo = `https://api.openweathermap.org/data/2.5/onecall?lat=${dataLat}&lon=${dataLon}&exclude=&appid=6212f09690f4fd274b9ba1da9141cbaa`
-// let locationInfo = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},US&limit=&appid=6212f09690f4fd274b9ba1da9141cbaa`
 let inputField = document.querySelector(".input");
 let searchBtn = document.querySelector(".button");
 let savedCity = document.querySelector(".city-searched");
 
 
 // local storage saving searched city
-searchBtn.addEventListener("click", function (event) {
+searchBtn.addEventListener("click", function(event) {
     event.preventDefault();
     let savedSearchedCity = localStorage.getItem("searchedCity");
     let searchedCity = [];
@@ -57,60 +54,72 @@ function getCity(dataLat, dataLon){
             return response.json();
         })
         .then(function(data){
+        removeImg();
         currentWeather(data.current);
         futureWeather(data.daily);
         })
 }
 
+function removeImg () {
+    document.querySelector("img").remove();
+}
+
 function currentWeather(current){
+    
+    let weatherMain = document.querySelector(".current-main")
     let foundCity = document.querySelector(".city-found");
     let cityTemp = document.querySelector(".temp");
     let cityWind = document.querySelector(".wind");
     let cityHumid = document.querySelector(".humid");
     let cityUvi = document.querySelector(".uvi");
-    // let foundIcon = document.querySelector(".found-icon")
-    // console.log(current.weather.icon)
-    // foundIcon.append("img")` src = "http://openweathermap.org/img/wn/${current.weather.icon}.png"`
+    let weatherIcon = document.createElement('img');
+
+    weatherIcon.src =`http://openweathermap.org/img/wn/${current.weather[0].icon}.png`;
+    weatherMain.append(weatherIcon);
     foundCity.textContent = `${inputField.value} ${moment().format("M/D/YYYY")}`;
     cityTemp.textContent = `Temp: ${current.temp}  °F`;
     cityWind.textContent = `Wind: ${current.wind_speed} MPH`;
     cityHumid.textContent = `Humidity: ${current.humidity} %`;
     cityUvi.textContent = `UV Index: ${current.uvi}`;
-    
-    // empty IMG src= url on openweather fix w/ icon code
 
 }
 
 function futureWeather(daily){ 
-    let fiveDay = document.querySelector(".five")
-    fiveDay.textContent = "Five Day Forcast:"
-    // let truck = moment date ${moment().format("M/D/YYYY").add(i, "d")
+    let fiveDay = document.querySelector(".fiveFive");
+    let dayFive = document.querySelector(".five");
+    fiveDay.textContent = "Five Day Forcast:";
+    
+
     for (let i = 1; i< 6; i++) {
+
         // let futureDate = document.createElement("h2");
         // futureDate.setAttribute("class", "dateFuture");
-
         // fiveDay.append(futureDate);
-
-        let futureTemp = document.createElement("p");
-        futureTemp.setAttribute('class', "tempFuture");
-        futureTemp.textContent = `Temp: ${i.temp.max} °F`
-        fiveDay.append(futureTemp);
-
-        let futureWind = document.createElement("p");
-        futureWind.setAttribute("class", "windFuture");
-        futureWind.textContent = `Wind: ${i.wind_speed} MPH`;
-        fiveDay.append(futureWind);
-
-        let futureHumid = document.createElement("p");
-        futureHumid.setAttribute("class", "humidFuture");
-        futureHumid.textContent = `Humidity: ${i.humidity} %`
-        fiveDay.append(futureHumid)
         
+        let futureDiv = document.createElement('div');
+        let futureTemp = document.createElement("p");
+        let futureWind = document.createElement("p");
+        let futureHumid = document.createElement("p");
+        let weatherIcon = document.createElement('img');
+
+        futureDiv.setAttribute("class", "futureDivBox");
+        futureTemp.setAttribute('class', "tempFuture");
+        futureWind.setAttribute("class", "windFuture");
+        futureHumid.setAttribute("class", "humidFuture");
+        weatherIcon.setAttribute("class", "updatedIcon");
+
+        futureTemp.textContent = `Temp: ${daily[i].temp.max} °F`;
+        futureDiv.append(futureTemp);
+        
+        futureWind.textContent = `Wind: ${daily[i].wind_speed} MPH`;
+        futureDiv.append(futureWind);
+
+        futureHumid.textContent = `Humidity: ${daily[i].humidity} %`;
+        futureDiv.append(futureHumid);
+
+        weatherIcon.src = `http://openweathermap.org/img/wn/${daily[i].weather[0].icon}.png`;
+
+        dayFive.append(futureDiv);
+        futureDiv.append(weatherIcon);
     }
-    console.log(daily[4]) //date
-    console.log(daily[1].temp.max)
-    console.log(daily[2].wind_speed)
-    console.log(daily[3].humidity)
-
-
 }
